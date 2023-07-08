f55535d6c6e5a44e54b85c91a22cdf7e49c0c307
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-07-08 10:47:32 -0700

    updates lab

--- lib/looping.py
+++ lib/looping.py
@@ -11,22 +11,43 @@ def happy_new_year():
         
         
 
-def square_integers(int_list):
-    squared_list = []
-    for num in int_list:
-        squared_list.append(num ** 2)
-    return squared_list
+# def square_integers(int_list):
+#     squared_list = []
+#     for num in int_list:
+#         squared_list.append(num ** 2)
+#     return squared_list
 
+def square_integers(int_list):
+    squared_integer_list = [num**2 for num in int_list]
+    return squared_integer_list
+
+result = square_integers([1, 2, 3, 4])
+print(result)
 
     
+# def fizzbuzz():
+#     for i in range(1, 101):
+#         if not i % 5 and not i % 3:
+#             print("FizzBuzz")
+#         elif not i % 5:
+#             print("Buzz")
+#         elif not i % 3:
+#             print("Fizz")
+#         else:
+#             print(i)
+
+
+
 def fizzbuzz():
     for i in range(1, 101):
-        if not i % 5 and not i % 3:
-            print("FizzBuzz")
-        elif not i % 5:
+        if i % 15 == 0:
+            print('FizzBuzz')
+        elif i % 5 == 0:
             print("Buzz")
-        elif not i % 3:
+        elif i % 3 == 0:
             print("Fizz")
         else:
             print(i)
 
+# result = fizzbuzz(101)
+# print(fizzbuzz)
